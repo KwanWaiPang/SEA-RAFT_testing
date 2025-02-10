db88e378102b6e77b4130b72aef65e5e010389f3
GUAN Weipeng <44108932+KwanWaiPang@users.noreply.github.com>
2025-02-10 11:58:30 +0800

    开始comment

--- custom.py
+++ custom.py
@@ -117,14 +117,16 @@ def demo_custom(model, args, device=torch.device('cuda')):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--cfg', help='experiment configure file name', required=True, type=str)
-    parser.add_argument('--path', help='checkpoint path', type=str, default=None)
+    parser.add_argument('--path', help='checkpoint path', type=str, default=None) #导入预训练模型
     parser.add_argument('--url', help='checkpoint url', type=str, default=None)
     parser.add_argument('--device', help='inference device', type=str, default='cpu')
     args = parse_args(parser)
     if args.path is None and args.url is None:
-        raise ValueError("Either --path or --url must be provided")
+        raise ValueError("Either --path or --url must be provided")#必须提供其中一个
+    
+    # Load model
     if args.path is not None:
-        model = RAFT(args)
+        model = RAFT(args)#加载模型
         load_ckpt(model, args.path)
     else:
         model = RAFT.from_pretrained(args.url, args=args)
